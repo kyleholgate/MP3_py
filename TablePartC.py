0961bafbce1a18a401238b335e1743c252acfbd4
root <kyle.holgate@gmail.com>
2020-04-06 03:42:31 +0000

    D

--- TablePartC.py
+++ TablePartC.py
@@ -3,16 +3,16 @@ import csv
 
 
 db = hb.Connection()
-table = db.table('power')
+table = db.table('powers')
 
 file = open('input.csv')
 data = csv.reader(file)
 
 for r in data:
-        table.put(r[0], {b'personal:hero': r[1],
-                                 b'personal:power': r[2],
-                                                      b'professional:name': r[3],
-                                                                           b'professional:xp': r[4],
-                                                                                                b'custom:color': r[5]} 
+	table.put(r[0], {b'personal:hero': r[1],
+                         b'personal:power': r[2],
+                         b'professional:name': r[3],
+                         b'professional:xp': r[4],
+                         b'custom:color': r[5]}) 
                                                                                                 
 db.close()
--- TablePartD.py
+++ TablePartD.py
@@ -2,21 +2,31 @@ import happybase as hb
 
 # DON'T CHANGE THE PRINT FORMAT, WHICH IS THE OUTPUT
 # OR YOU WON'T RECEIVE POINTS FROM THE GRADER
+db = hb.Connection()
+table = db.table('powers')
 
-hero = ???
-power = ???
-name = ???
-xp = ???
-color = ???
+r = table.row(b'row1')
+
+hero = r[b'personal:hero']
+power = r[b'personal:power']
+name = r[b'professional:name']
+xp = r[b'professional:xp']
+color = r[b'custom:color']
 
 print('hero: {}, power: {}, name: {}, xp: {}, color: {}'.format(hero, power, name, xp, color))
 
-hero = ???
-color = ???
+r = table.row(b'row19')
+
+hero = r[b'personal:hero']
+color = r[b'custom:color']
 
 print('hero: {}, color: {}'.format(hero, color))
 
-hero = ???
-name = ???
-color = ???
+r = table.row(b'row1')
+
+hero = r[b'personal:hero']
+name = r[b'professional:name']
+color = r[b'custom:color']
 print('hero: {}, name: {}, color: {}'.format(hero, name, color))
+
+db.close()
