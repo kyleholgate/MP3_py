1cd4482a16a6a6297300b3ed5fbc161075d26e6b
Kyle Holgate <kyle.holgate@gmail.com>
2020-04-05 22:35:49 -0400

    C

--- TablePartC.py
+++ TablePartC.py
@@ -1,3 +1,18 @@
 import happybase as hb
 import csv
 
+
+db = hb.Connection()
+table = db.table('power')
+
+file = open('input.csv')
+data = csv.reader(file)
+
+for r in data:
+        table.put(r[0], {b'personal:hero': r[1],
+                                 b'personal:power': r[2],
+                                                      b'professional:name': r[3],
+                                                                           b'professional:xp': r[4],
+                                                                                                b'custom:color': r[5]} 
+                                                                                                
+db.close()
