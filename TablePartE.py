5e93c1739316098a1fdb8472121850dc94a3cacd
root <kyle.holgate@gmail.com>
2020-04-06 13:11:22 +0000

    finish

--- TablePartE.py
+++ TablePartE.py
@@ -3,6 +3,9 @@ import happybase as hb
 # DON'T CHANGE THE PRINT FORMAT, WHICH IS THE OUTPUT
 # OR YOU WON'T RECEIVE POINTS FROM THE GRADER
 
-for key, data in ???:
+db = hb.Connection()
+table = db.table('powers')
+
+for key, data in table.scan():
     print('Found: {}, {}'.format(key, data))
 
--- TablePartF.py
+++ TablePartF.py
@@ -3,14 +3,20 @@ import happybase as hb
 # DON'T CHANGE THE PRINT FORMAT, WHICH IS THE OUTPUT
 # OR YOU WON'T RECEIVE POINTS FROM THE GRADER
 
-color = ???
-name = ???
-power = ???
+db = hb.Connection()
+table = db.table('powers')
 
-color1 = ???
-name1 = ???
-power1 = ???
+for key, data in table.scan():
+	color = data['custom:color']
+	name = data['professional:name']
+	power = data['personal:power']
 
-print('{}, {}, {}, {}, {}'.format(name, power, name1, power1, color))
+	for key1, data1 in table.scan():
+		if key != key1 and color == data1['custom:color'] and name != data1['professional:name']:
+			color1 = data1['custom:color']
+			name1 = data1['professional:name']
+			power1 = data1['personal:power']
 
+			print('{}, {}, {}, {}, {}'.format(name, power, name1, power1, color))
 
+db.close()
